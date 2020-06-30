92d4a99380b5aa14c9ac98c83c83074f4ca35fed
Elizabeth Kong <elizabethtkong@gmail.com>
2020-07-01 07:24:16 +0800

    Add song-list.js

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -16,6 +16,11 @@
 
     song.element.on('click' , event => {
       player.playPause(song);
+      $('button#play-pause').attr('playState', player.playState);
+    });
+
+    $('button#next').on('click', function() {
+
     });
 
     $('#song-list').append(song.element);
